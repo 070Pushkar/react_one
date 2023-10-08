b9d9e2cee23afa4d438d509ff86c057ba96d6ec4
070Pushkar <111866894+070Pushkar@users.noreply.github.com>
2023-10-08 16:35:52 +0530

    This is the my first react project

---
+++ one/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+     <div>
+        <h1>Alex</h1>
+        <p>@all right reserved</p>
+    </div>  
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://linkedin.com/in/pushkar-srivastav-85905b244" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://www.instagram.com/pushkarsrivastav95/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/070Pushkar" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer;
